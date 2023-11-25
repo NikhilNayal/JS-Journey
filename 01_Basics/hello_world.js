fbf0ab3c362e8b0563e09736df309f4ba061ace1
NikhilNayal <45266189+NikhilNayal@users.noreply.github.com>
2023-11-25 06:00:42 +0000

    new updates

--- 01_Basics/hello_world.js
+++ 01_Basics/hello_world.js
@@ -1,1 +1,4 @@
+// Install Liver Sever and then open the project with live server
 console.log("Hello World")
+document.write("Hello World!!")
+alert("yoooo")
---
+++ 02_Datatypes/primitive_dt.js
@@ -0,0 +1,8 @@
+// Primitive types
+let firstName = "Nikhil"
+let lastName = 'Nayal'
+const age = 40
+age = 90
+console.log(firstName, "is of type ",typeof(firstName))
+console.log(lastName, "is of type ",typeof(lastName))
+console.log(age, typeof(age))
