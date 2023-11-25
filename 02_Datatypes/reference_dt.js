80f3600365f96f6e293e4680a62b877c3af8f339
NikhilNayal <45266189+NikhilNayal@users.noreply.github.com>
2023-11-25 10:09:48 +0000

    updated

--- 02_Datatypes/reference_dt.js
+++ 02_Datatypes/reference_dt.js
@@ -4,7 +4,7 @@ Objects : are the collection  of properties
 where each property is the key: value pair
 
 */
-
+console.log("----------------------------Lets learn Objects-----------------")
 let human = {
     firstName: "Nikhil",
     lastName:"Nayal",
@@ -14,3 +14,31 @@ let human = {
 console.log(human , typeof(human))
 console.log(human["firstName"],typeof(human["firstName"]))
 console.log(human.firstName , typeof(human.firstName))
+
+
+// array
+// Array are typ[e of objects that stores a collection of value
+console.log("----------------------------Lets learn Arrays-----------------")
+let itemList = ["red","green",2,50,true]
+console.log(itemList,typeof(itemList))
+let idx = 4
+if (itemList[idx]){
+    console.log("5th item in the list is",itemList[idx],"and has a type ",typeof(itemList[idx]))
+    console.log("length of the itemList is ",itemList.length)
+}
+
+
+for(let item =0;item<itemList.length;item++){
+    console.log("item in the list is",itemList[item],"and has a type ",typeof(itemList[item]))
+}
+console.log("----------------------------Lets learn functions-----------------")
+// Function are type of objects that execute the code
+function add(num1,num2){
+    return num1 + num2
+}
+console.log(add(5,8),typeof(add))
+
+const sub = function(num1,num2){
+    return num1 - num2
+}
+console.log(sub(19,8),typeof(sub))
